6c2a0065b5a73a01c59445202bee111d2c512c40
Kaushal Bakraniya <kaushalbakraniya90@gmail.com>
2025-08-28 22:28:34 +0530

    added helloworld.js

---
+++ August2025/helloworld.js
@@ -0,0 +1,4 @@
+var mahamantra = 'hare krishna hare krishna krishna krishna hare hare';
+mahamantra += 'hare ram hare ram ram ram hare hare';
+console.log(mahamantra);
+console.log('Chant hare krishna and be happy :)')
